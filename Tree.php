474c7535393a2a7f4858631a1a55856dc4304106
Mohsen Khahani <mkhahani@gmail.com>
2013-05-22 15:36:16 +0430

    initial release of phpTree

---
+++ Tree.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * phpTree - A PHP class to build XHTML tree from a data array
+ * © 2011-2013 Mohsen Khahani
+ *
+ * Licensed under The MIT license.
+ * Created on November 1, 2011
+ *
+ * For more information visit: http://mohsenkhahani.ir/phptree
+ */
+
+class Tree
+{
+    /**
+     * Class version
+     *
+     * @var     string
+     * @access  public
+     */
+    var $Version = '2.0';
+
+    /**
+     * Class constructor
+     *
+     * @access  public
+     * @param   array   $data       Tree data [id, parent, text]
+     * @param   mixed   $callback   Callback function to build a node
+     *                              String function name or Array(class name, function name)
+     *                              For more info see PHP's call_user_func()
+     * @return  void
+     */
+    function Tree($data, $callback = null)
+    {
+        $this->data = $data;
+        $this->callback = $callback;
+    }
+
+    /**
+     * Fetches children data of the givven node
+     *
+     * @access  private
+     * @param   int  $id    Node ID
+     * @return  array   Children data
+     */
+    function getChildren($id)
+    {
+        $res = array();
+        foreach ($this->data as $node) {
+            if ($node[1] === $id) {
+                $res[] = $node;
+            }
+        }
+        return $res;
+    }
+
+    /**
+     * Builds sub tree from givven nodes data
+     *
+     * @access  private
+     * @param   array   $data   Nodes data
+     * @return  string  XHTML sub tree
+     */
+    function buildNodes($data)
+    {
+        $tree = '';
+        foreach ($data as $row) {
+            $childNodes = '';
+            $children = $this->getChildren($row[0]);
+            if (count($children) > 0) {
+                $childNodes = $this->buildNodes($children);
+            }
+
+            if ($this->callback) {
+                $tree .= call_user_func($this->callback, $row, $childNodes);
+            } else {
+                $tree .= '<li>' . $row[2];
+                if (!empty($childNodes)) {
+                    $tree .= '<ul>' . $childNodes . '</ul>';
+                }
+                $tree .= '</li>';
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * Builds the tree
+     *
+     * @access  public
+     * @return  string  XHTML tree
+     */
+    function get()
+    {
+        $rootNodes = $this->getChildren(0);
+        return $this->buildNodes($rootNodes);
+    }
+}
---
+++ sample.php
@@ -0,0 +1,53 @@
+<?php 
+    $treeData = array(
+        array(1, 0, '1'),
+        array(2, 1, '1.1'),
+        array(3, 0, '2'),
+        array(4, 1, '1.2'),
+        array(5, 1, '1.3'),
+        array(6, 4, '1.2.1'),
+        array(7, 4, '1.2.2'),
+        array(8, 4, '1.2.3'),
+        array(9, 0, '3'),
+        array(10, 9, '3.1'),
+        array(11, 10, '3.1.1'),
+        array(12, 11, '3.1.1.1'),
+        array(13, 11, '3.1.1.2'),
+        array(14, 11, '3.1.1.3'),
+        array(15, 13, '3.1.1.2.1'),
+    );
+
+    function getNode($data, $children) {
+        $node = '<li><div>' . $data[2] . '</div>';
+        if (!empty($children)) {
+            $node .= '<ul>' . $children . '</ul>';
+        }
+        $node .= '</li>';
+        return $node;
+    }
+
+    require_once 'Tree.php';
+    $tree1 = new Tree($treeData);
+    $tree2 = new Tree($treeData, 'getNode');
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <title>phpTree v2.0</title>
+</head>
+<style type="text/css">
+  table {margin:100px auto;}
+  td {border:1px solid silver; padding:10px;}
+</style>
+<body>
+  <table>
+    <tr><th>Tree without callback function</th><th>Tree using callback function</th></tr>
+    <tr>
+      <td><ul class="tree"><?php echo $tree1->get(); ?></ul></td>
+      <td><ul class="tree"><?php echo $tree2->get(); ?></ul></td>
+    </tr>
+
+  </table>
+</body>
+</html>
